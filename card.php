8ad4fc0de919e16a0a0f0e6bf4e4755a3f0660cb
Emil <pillar_box@bk.ru>
2019-10-02 17:15:55 +0300

    change table size
    set the table size to 16 rows

--- card.php
+++ card.php
@@ -3,7 +3,7 @@ session_start();
 if (!isset($_SESSION["ldap"])){
 	header("location: index.php");
 }
-$rows=17;
+$rows=16;
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
